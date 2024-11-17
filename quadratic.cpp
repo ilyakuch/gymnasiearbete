609c0255f9f896f40dd32a93e296df3929cb7c7c
ilyakuch <ilyakuch2006@gmail.com>
2024-11-18 00:44:21 +0100

    asdf

--- quadratic.cpp
+++ quadratic.cpp
@@ -1,10 +1,12 @@
 #include <vector>
 #include <iostream>
+#include <chrono>
+#include <utility>
 
-int quadratic(std::vector<int>& arr) {
+int quadratic(std::vector<int>& dataset) {
     int pairs = 0;
-    for (size_t i = 0; i < arr.size(); ++i) {
-        for (size_t j = i + 1; j < arr.size(); ++j) {
+    for (size_t i = 0; i < dataset.size(); ++i) {
+        for (size_t j = i + 1; j < dataset.size(); ++j) {
             pairs++;
         }
     }
@@ -12,8 +14,40 @@ int quadratic(std::vector<int>& arr) {
 }
 
 int main() {
-    std::vector<int> arrr = {1, 2, 3, 4, 5};
-    quadratic(arrr);
+    
+    const int max_size = 1000;
+    const int increment = 100;
+    const int max_runtime = 60;
+    
+    int iteration = 1;
+    std::vector<std::pair<int, int>> results;
+    
+
+    while (iteration > max_size) {
+
+        std::vector<int> dataset;
+
+        for (size_t i = 0; i < iteration; i++) {
+            dataset.push_back(i);
+        }
+
+        auto start = std::chrono::high_resolution_clock::now();
+        int iteration_return = quadratic(dataset);
+        auto end = std::chrono::high_resolution_clock::now();
+
+        std::chrono::duration<double> measurment = end - start;
+
+        std::cout << measurment.count() << "Iteration: " << iteration << " --- Returned: " << iteration_return << std::endl;
+
+        results.push_back({iteration, measurment.count()});
+
+        iteration = iteration + increment;
+
+    }
+
+    for (const auto& i : results) {
+        std::cout << i.first << ", " << i.second << std::endl;
+    }
     
     return 0;
 }
